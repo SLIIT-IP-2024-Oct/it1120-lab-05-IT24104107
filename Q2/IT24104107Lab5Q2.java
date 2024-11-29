a44ef17651c0a1c189d39958398ef646e687806e
IT24104107 <chathuminidilsha6@gmail.com>
2024-11-29 14:04:59 +0530

    Add files via upload

---
+++ Q2/IT24104107Lab5Q2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+publi class IT24104107Lab5Q2 {
+ public static void main (String [] args ) {
+  Scanner lab = newScanner(System.in);
+  System.out.println("Enter the number of new members introduced");
+  int members=lab.nextInt();
+    if (members<0){
+    System.out.println("Input must be a number 0 or greater");
+    } else { 
+     switch (member) {
+      case 0 :
+     System.out.println("No Prize");
+      break;
+      case 2 :
+    System.out.println("Prize is an Umbrella ");
+      break;
+      case 3:
+    System.out.println("Prize is a Bag");
+      break;
+      case 4:
+    System.out.println("Prize ie a Travelling Chair");
+       break;
+       default :
+    System.out.println("Prize is a Headphone");
+            }
+         }
+ }
+} 
